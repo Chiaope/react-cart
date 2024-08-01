c7ec0503eb6bffd8944ffbd40b0394189477f58a
gui kang <gui_kang@hotmail.com>
2024-08-01 17:26:07 +0800

    changed state management to be used by context

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import Header from './components/Header.jsx';
 import Shop from './components/Shop.jsx';
 import { DUMMY_PRODUCTS } from './dummy-products.js';
+import { CartContext } from './store/CartContext,jsx';
 
 function App() {
   const [shoppingCart, setShoppingCart] = useState({
@@ -65,14 +66,17 @@ function App() {
     });
   }
 
+  const CartContextValue = {
+    items: shoppingCart.items,
+    onAddItemToCart: handleAddItemToCart,
+    onUpdateCartItemQuantity: handleUpdateCartItemQuantity
+}
+
   return (
-    <>
-      <Header
-        cart={shoppingCart}
-        onUpdateCartItemQuantity={handleUpdateCartItemQuantity}
-      />
-      <Shop onAddItemToCart={handleAddItemToCart} />
-    </>
+    <CartContext.Provider value={CartContextValue}>
+      <Header />
+      <Shop />
+    </CartContext.Provider>
   );
 }
 
